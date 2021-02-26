30e5d5029ac7c54dc55d2c05ab6bb28171783643
jaeho3 <hojoh@sheridancollege.ca>
2021-02-26 10:21:02 -0500

    edited code

---
+++ src/midtermexam_versiona_extensioncode/TestUserprofile.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package midtermexam_versiona_extensioncode;
+
+import java.util.Scanner;
+/**
+ *
+ * @author johnt
+ */
+public class TestUserprofile {
+    public static void main(String[] args)
+    {
+        int userCount=0;//keep track of number of users for array
+        Scanner sc = new Scanner(System.in);
+        System.out.println("please enter your desired user name:");
+        String userName = sc.nextLine();
+        
+        String genre="";
+
+        {
+            System.out.println("Please Select your favourite Genre.");
+            System.out.println("Action, Adventure, Drama, Comedy,");            
+            genre = sc.nextLine();
+            
+        
+        UserID newUserID = new UserID(userName, genre);
+        users[userCount] = newUser;//add the new user to the user list
+        userCount++;
+        System.out.println("New User Added");
+        System.out.println("UserName: " + userID);
+
+    }//end run method        
+    }
+}
